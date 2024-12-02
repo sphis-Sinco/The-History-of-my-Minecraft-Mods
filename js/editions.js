aa49d7a0e0a437cb3ed5a7313f6fbcd85a357c77
IdealistCat <mattallan.h@gmail.com>
2024-12-01 20:33:07 -0600

    images

--- js/editions.js
+++ js/editions.js
@@ -3,7 +3,7 @@ const EDITIONS = [
     "title": "Improvists (Mod)",
     "description": "The main mod",
     "firstMessage": "The Mod is Released",
-    "logo": "logo_java.png",
+    "logo": "default.png",
 
     "versions": [
       {
@@ -11,9 +11,10 @@ const EDITIONS = [
         "description": "Money and an Ancient Structure",
         "type": "major",
         "date": "2024-12-01",
-        "icon": "versions/version_unknown.png",
+        "icon": "versions/version_0.1.png",
         "mainFeatures": [
           { "text": "Iron and Gold Coins" },
+          { "text": "Iron and Gold Coin Nuggets" },
           { "text": "Sandstone Housing Structure" }
         ],
         "minorFeatures": [
